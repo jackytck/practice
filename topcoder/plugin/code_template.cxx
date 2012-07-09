16d49e040e9c9044092dc9914cd635ab03d8d1ed
Jacky Tang <jackytck@gmail.com>
2012-07-09 22:57:35 +0800

    c++ code template

---
+++ topcoder/plugin/code_template.cxx
@@ -0,0 +1,51 @@
+$BEGINCUT$
+/*
+$PROBLEMDESC$
+*/
+$ENDCUT$
+#line $NEXTLINENUMBER$ "$FILENAME$"
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+#define sz size()
+#define PB push_back
+#define clr(x) memset(x, 0, sizeof(x))
+#define forn(i,n) for(__typeof(n) i = 0; i < (n); i++)
+#define ford(i,n) for(int i = (n) - 1; i >= 0; i--)
+#define forv(i,v) forn(i, v.sz)
+#define For(i, st, en) for(__typeof(en) i = (st); i < (en); i++)
+
+using namespace std;
+
+class $CLASSNAME$
+{
+	public:
+        $RC$ $METHODNAME$($METHODPARMS$)
+        {	 	 
+        } 
+    $TESTCODE$ 
+}; 
+
+$BEGINCUT$ 
+int main()
+{
+	$CLASSNAME$ ___test; 
+    ___test.run_test(-1); 
+}
+$ENDCUT$ 
